d8d63f480b44889230d4f29ea45c1fe36bc85705
alainasaji5.io <140042385+alainasaji5@users.noreply.github.com>
2024-05-15 16:56:08 +0530

    Add files via upload

---
+++ TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.TravelPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
